0b1df50a62cb8145f1a8c9e6b358df617cc48eda
Doron Cohen <me@doron.se>
2024-07-14 22:13:55 +0200

    feat: get changed packages

--- core/pkgtree.go
+++ core/pkgtree.go
@@ -2,8 +2,42 @@ package core
 
 import (
 	"context"
+
+	"github.com/doron-cohen/pkgtree/packages"
+	"golang.org/x/exp/slices"
 )
 
 func GetChangedPackages(ctx context.Context, differType DifferType) ([]string, error) {
-	return GetChangedFiles(ctx, differType, ".")
+	files, err := GetChangedFiles(ctx, differType, ".")
+	if err != nil {
+		return nil, err
+	}
+
+	pkgs := make([]string, 0, len(files))
+	for _, file := range files {
+		pkg, err := packages.GetFilePackageName(ctx, file)
+		if err != nil {
+			return nil, err
+		}
+
+		pkgs = append(pkgs, pkg)
+	}
+
+	return uniqueAndSort(pkgs), nil
+}
+
+func uniqueAndSort(pkgs []string) []string {
+	seen := make(map[string]struct{})
+	uniquePkgs := make([]string, 0, len(pkgs))
+	for _, pkg := range pkgs {
+		if _, ok := seen[pkg]; ok {
+			continue
+		}
+
+		seen[pkg] = struct{}{}
+		uniquePkgs = append(uniquePkgs, pkg)
+	}
+
+	slices.Sort(uniquePkgs)
+	return uniquePkgs
 }
---
+++ packages/packages.go
@@ -0,0 +1,40 @@
+package packages
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+
+	"golang.org/x/tools/go/packages"
+)
+
+func GetFilePackageName(ctx context.Context, filePath string) (string, error) {
+	cfg := &packages.Config{
+		Context: ctx,
+		Mode:    packages.NeedName | packages.NeedFiles | packages.NeedModule,
+		Dir:     filepath.Dir(filePath),
+	}
+
+	pkgs, err := packages.Load(cfg, ".")
+	if err != nil {
+		return "", fmt.Errorf("failed to load package: %w", err)
+	}
+
+	if len(pkgs) == 0 {
+		return "", fmt.Errorf("no packages found")
+	}
+
+	return pkgs[0].PkgPath, nil
+}
+
+func getFilePackageName(filePath string, pkgs []*packages.Package) (string, error) {
+	for _, pkg := range pkgs {
+		for _, file := range pkg.GoFiles {
+			if file == filePath {
+				return pkg.PkgPath, nil
+			}
+		}
+	}
+
+	return "", fmt.Errorf("package not found")
+}
